acf4cfc6a63f38b0ec79458adda20a4cf47590dc
James Coliz <jcoliz@mail.com>
2016-11-24 12:46:20 -0800

    Jumping and score display

--- GameDay/Scenes/Chapter09.xaml.cs
+++ GameDay/Scenes/Chapter09.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -35,6 +36,10 @@ namespace GameDay.Scenes
         Sprite Player;
         Sprite Dark;
 
+        // Variables we want to display
+        Variable<int> PlayerHP = new Variable<int>(20);
+        Variable<int> CpuHP = new Variable<int>(30);
+
         public void Scene_Loaded(object sender, RoutedEventArgs args)
         {
             SetBackdrop("09/43.png");
@@ -45,10 +50,51 @@ namespace GameDay.Scenes
         private void Player_Loaded(Sprite me)
         {
             me.SetPosition(LeftEdge/2, BottomEdge/2);
+            me.SetVariable<double>("starty", me.Position.Y);
             me.SetCostume("09/18.png");
             me.SetRotationStyle(Sprite.RotationStyle.LeftRight);
             me.Show();
+            me.KeyPressed += Player_KeyPressed;
         }
+
+        double yspeed = 0;
+        double gravity = -4;
+
+        private void Player_KeyPressed(Sprite me, Windows.UI.Core.KeyEventArgs what)
+        {
+            var starty = me.GetVariable<double>("starty");
+
+            if (what.VirtualKey == Windows.System.VirtualKey.Left)
+            {
+                me.ChangeXby(-80);
+                if (me.Position.X < LeftEdge)
+                    me.SetX(LeftEdge);
+            }
+            if (what.VirtualKey == Windows.System.VirtualKey.Right)
+            {
+                me.ChangeXby(80);
+                if (me.Position.X > RightEdge)
+                    me.SetX(RightEdge);
+            }
+            if (what.VirtualKey == Windows.System.VirtualKey.Up && me.Position.Y == starty)
+            {
+                yspeed = 40;
+                Task.Run(async () => 
+                {
+                    me.ChangeYby(yspeed);
+                    while(me.Position.Y > starty)
+                    {
+                        await Delay(0.05);
+                        yspeed += gravity;
+                        me.ChangeYby(yspeed);
+                    }
+                    me.SetY(starty);
+
+                });
+
+            }
+        }
+
         private void Dark_Loaded(Sprite me)
         {
             me.SetPosition(RightEdge / 2, BottomEdge/2);
